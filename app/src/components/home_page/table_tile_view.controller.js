1c81e3fbd5c322b25fad8aece5898050f1798be2
asieduernest <asieduernest@yahoo.co.uk>
2018-05-16 16:02:22 -0400

    delete_one update and counter to disable delete_multiple button

--- app/src/components/home_page/table_tile_view.controller.js
+++ app/src/components/home_page/table_tile_view.controller.js
@@ -9,8 +9,11 @@
   function homePageTableController($scope,RecordService){
     var vm = $scope // vm is just a place holder synonymous to view_model
 
+    vm.marked_for_deletion_counter = 0;
     vm.delete_multiple = delete_multiple;
     vm.delete_one = delete_one;
+    vm.count_records_marked_for_deletion = count_records_marked_for_deletion;
+
 
 
     initializeRecords();
@@ -18,6 +21,8 @@
     function initializeRecords(){
       RecordService.getAllRecords().then(function(response){
         vm.records = response
+        vm.marked_for_deletion_counter = 0;
+        count_records_marked_for_deletion();
         console.log(response);
       },function(err){
         if (err){
@@ -27,6 +32,7 @@
     }
 
     function delete_multiple(){
+
       vm.records.forEach(function(record,index){
         console.log(record);
         if(record.hasOwnProperty('deletable') && record.deletable){
@@ -39,12 +45,33 @@
 
 
     function delete_one(record){
-      console.log('attempting delete');
+      console.log('attempting delete using delete_one()');
       RecordService.deleteRecord(record._id).then((response)=>{
+        alert('Delete operation successful');
         initializeRecords();
       },(err)=>{
         console.log(err);
       });
     }
+
+    /**
+    * [count_records_marked_for_deletion count records and store number of records marked for deletion in marked_for_deletion_counter]
+    * marked_for_deletion_counter == 0 means false therefore delete_button must be disabled
+    * @method count_records_marked_for_deletion
+    * @return {[type]}                          [description]
+    */
+    function count_records_marked_for_deletion(){
+      vm.marked_for_deletion_counter = 0;
+      vm.records.forEach(function(record,index){
+        console.log(record);
+        if(record.hasOwnProperty('deletable') && record.deletable){
+          vm.marked_for_deletion_counter ++;
+        }
+      });
+      console.log('marked_for_deletion_counter updated with count: ',vm.marked_for_deletion_counter);
+    }
+
+
+
   }
 })();
